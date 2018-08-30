6767876d80b2d528488ac086650bfe4c65b1778d
Geoffrey L. Angus <glangus@us.ibm.com>
2018-08-30 15:00:21 -0300

    added slides

--- scripts/playlist_scraper/scrape_script.js
+++ scripts/playlist_scraper/scrape_script.js
@@ -1,15 +1,24 @@
-table = document.body.querySelector("#pl-video-table")
-data  = table.querySelectorAll("tr")
+table = document.body.querySelector("ytd-playlist-video-list-renderer")
+data  = table.querySelectorAll("ytd-playlist-video-renderer")
 
 songs = []
 for (var i = 0; i < data.length; i++) {
-    title = data[i].getAttribute("data-title")
-    link = data[i].querySelector(".pl-video-title-link").getAttribute("href")
-    time = data[i].querySelector(".pl-video-time").innerText
+    link = data[i].querySelector("#content a").getAttribute("href")
+    title = data[i].querySelector("#content #video-title").innerText
+    artist = data[i].querySelector("#content #metadata yt-formatted-string").getAttribute("title")
+    time = data[i].querySelector("#content ytd-thumbnail-overlay-time-status-renderer span").innerText
     songs.push({
         title,
+        artist,
+        time,
         link,
-        time
+
     })
 }
 
+for (var i = 0; i < songs.length; i++) {
+    console.log("title: " + songs[i].title)
+    console.log("artist: " + songs[i].artist)
+    console.log("time: " + songs[i].time)
+    console.log("link: " + songs[i].link)
+}
