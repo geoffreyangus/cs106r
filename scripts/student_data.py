9971452697aa70812bed19c7f0d281345a01fd35
Sabri Eyuboglu <sabrieyuboglu@MacBook-Air-de-Fabricio.local>
2018-09-02 17:07:20 -0300

    Implemented the exercise completion rates.

--- scripts/student_data.py
+++ scripts/student_data.py
@@ -4,6 +4,32 @@ Breaks up student data into classes and presents in plots.
 from csv import DictReader, DictWriter  
 import numpy as np
 
+class Class:
+
+    def __init__(self, name, exercises, projects, challenges, students = []):
+        self.name = name
+
+        self.students = students
+
+        self.exercises = {exercise: round(np.mean([student.exercises[exercise] for student in students]), 3)
+                                    for exercise in exercises}
+        self.projects = {project: round(np.mean([student.projects[project] for student in students]), 3)
+                                    for project in projects}
+        self.challenges = {challenge: round(np.mean([student.challenges[challenge] for student in students]), 3)
+                                    for challenge in challenges}
+ 
+    def to_dict(self):
+        row = {'_name': self.name, 
+               '_num_students': len(self.students),
+               'exercises': round(np.mean([student.exercise_completion for student in self.students]), 3),
+               'projects': round(np.mean([student.project_completion for student in self.students]), 3),
+               'challenges': round(np.mean([student.challenge_completion for student in self.students]), 3)} 
+        row.update(self.exercises)
+        row.update(self.projects)
+        row.update(self.challenges)
+        return row
+
+
 class Student:
     ''' Class for one student in the course.  
     '''
@@ -53,6 +79,11 @@ class ClassData:
         with open(completion_filename, 'r') as completion_file:
             raw_data = DictReader(completion_file)
             misses = 0
+
+            exercises = [field for field in raw_data.fieldnames if 'Exercise' in field and 'DRAFT'not in field]
+            projects = [field for field in raw_data.fieldnames if 'Project' in field and 'DRAFT'not in field]
+            challenges = [field for field in raw_data.fieldnames if 'Challenge' in field and 'DRAFT'not in field]
+
             for row in raw_data:
                 email = row['email']
                 if email in bj_contact_data:
@@ -66,8 +97,10 @@ class ClassData:
                     row['class'] = 'none'
                 
                 student = Student(row)
-                self.classes.setdefault(student.class_name, {}).setdefault('students', []).append(student)
                 self.students[student.email] = student
+                self.classes.setdefault(student.class_name, []).append(student)
+            self.classes = {class_name: Class(class_name, exercises, projects, challenges, students) 
+                            for class_name, students in self.classes.iteritems()}
             print '------------------------------------------'
             print 'Found ', len(self.classes), ' classes.'
             print 'Found ', len(self.students), ' students.'
@@ -85,19 +118,14 @@ class ClassData:
 
     def output_class_data(self, filename):
         with open(filename, 'w') as output_file:
-            fieldnames = ['name', 'num_students', 'exercises', 'projects', 'challenges']
+            fieldnames = self.classes.values()[0].to_dict().keys()
+            fieldnames.sort(reverse = True)            
             writer = DictWriter(output_file, fieldnames=fieldnames)
             writer.writeheader()
             class_names = self.classes.keys()
             class_names.sort()
-            for class_name in class_names:
-                students = self.classes[class_name]['students']
-                row = {'name': class_name, 
-                       'num_students': len(students),
-                       'exercises': round(np.mean([student.exercise_completion for student in students]), 3),
-                       'projects': round(np.mean([student.project_completion for student in students]), 3),
-                       'challenges': round(np.mean([student.challenge_completion for student in students]), 3)} 
-                writer.writerow(row)
+            for class_name in class_names:               
+                writer.writerow(self.classes[class_name].to_dict())
 
 
 def main():
